7d8157b9a49a977de6f65142bc1ef2d2b9e50467
noah <gaoyong@xatu.edu.cn>
2019-01-07 17:06:22 +0800

    20190107 Signed-off-by: noah <gaoyong@xatu.edu.cn>

---
+++ test.py
@@ -0,0 +1,23 @@
+# _*_coding:utf-8_*_
+# Author : oracle12c
+# Time   : 2019/1/7 15:34
+# File   : test.py
+# IDE    : PyCharm
+
+person = ['tony', ['savings', 100]]
+
+p1 = person[:]
+p2 = person[:]
+
+print(p1, '***', p2)
+
+p1[0] = 'tom'
+p2[0] = 'lode'
+
+p1[1][1] = 50
+
+print(p1, '***', p2)
+
+
+
+
---
+++ tuple.py
@@ -0,0 +1,11 @@
+# _*_coding:utf-8_*_
+# Author : oracle12c
+# Time   : 2019/1/7 16:59
+# File   : tuple.py
+# IDE    : PyCharm
+
+# 元组-被称为只读列表，只能查、切片，不能修改。
+names = ('tony', 'jone', 'god')
+# 元组两个方法，count和index
+
+names.count(names)
